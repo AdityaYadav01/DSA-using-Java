00e7fbaa5363cfeda03d97ea73c7d5fff34e258c
Aditya Yadav <adityayadav44556@gmail.com>
2024-08-25 21:26:43 +0530

    Largest of 3 Numbers

--- Loops/src/Problem1.java
+++ Loops/src/Problem1.java
@@ -22,6 +22,16 @@ public class Problem1 {
             System.out.println(num3 + " is the largest number");
         }
 
+        // Another Method
+        int max = num1;
+        if(num2>max){
+            max = num2;
+        }
+        if(num3>max){
+            max = num3;
+        }
+        System.out.println(max);
+
 
 
     }
