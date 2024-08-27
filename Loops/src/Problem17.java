a5873c6c9079177da3dd8324fb4dfce6314ab623
Aditya Yadav <adityayadav44556@gmail.com>
2024-08-27 14:27:02 +0530

    Pattern Problem-3

--- Loops/src/Problem17.java
+++ Loops/src/Problem17.java
@@ -10,7 +10,7 @@ public class Problem17 {
         int count = 1;
         for (int i = 1; i <=5 ; i++) {
             for(int j = 1; j<=i; j++){
-                System.out.print(count+ " ");
+                System.out.printf("%02d ",count);
                 count++;
             }
             System.out.println();
