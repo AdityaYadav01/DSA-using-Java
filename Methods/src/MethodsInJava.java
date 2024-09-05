595629f67513941e3759dd9bfb9509bed8c5f9c0
Aditya Yadav <adityayadav44556@gmail.com>
2024-09-05 21:50:43 +0530

    Methods in Java

--- Methods/src/MethodsInJava.java
+++ Methods/src/MethodsInJava.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 
 public class MethodsInJava {
     public static void main(String[] args) {
-        sum();
+        int ans = sum2();
+        System.out.println("The Sum is: "+ans);
+        String msg = greet();
+        System.out.println(msg);
+
 
     }
     static void sum(){                           // access modidifier, return type , Name of the method()
@@ -17,5 +21,26 @@ public class MethodsInJava {
         int num2 = in.nextInt();
 
         System.out.println("THe Sum is : "+ (num1+num2));
+    }
+
+    //Return the value
+    static int sum2(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = in.nextInt();
+
+        System.out.print("Enter the Second number: ");
+        int num2 = in.nextInt();
+
+        int sum2 = num1+num2;
+        return sum2;   // Method will stop working here
+    }
+
+    // Return a String
+    static String greet(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your message: ");
+        String message = in.nextLine();
+        return message;
     }
 }
