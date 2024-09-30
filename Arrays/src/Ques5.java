7fa40f9bdfdab978005236a8ebbf07607d1187c8
Aditya Yadav <adityayadav44556@gmail.com>
2024-09-30 21:04:55 +0530

    Rotating an array

--- Arrays/src/Ques5.java
+++ Arrays/src/Ques5.java
@@ -1,13 +1,17 @@
+import java.util.Arrays;
+
 // Rotating an array
 public class Ques5 {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
-        //int[] rotating = new int[100];
 
-        for (int i = arr.length-1; i >= 0 ; i--) {
-            int[] rotating = new int[]{arr[i]};
+        int temp = arr[arr.length-1];
 
-
+        for (int i = arr.length-2; i >=0 ; i--) {
+            arr[i+1] = arr[i];
         }
+        arr[0]=temp;
+        System.out.println(Arrays.toString(arr));
+
     }
 }
